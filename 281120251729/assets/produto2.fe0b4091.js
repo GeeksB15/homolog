d664ed5116b374e521f89da470d0e9af1b0d9a35
Jean P Franco <jnpefo@gmail.com>
2025-11-28 17:31:02 -0300

    2025.1128.1729

---
+++ 281120251729/assets/produto2.fe0b4091.js
@@ -0,0 +1,1 @@
+var o="/homolog/281120251729/assets/produto2.7b99a26f.jpg";export{o as _};
---
+++ 281120251729/assets/utils.7da89468.js
@@ -0,0 +1,1 @@
+import{b as s,aH as l}from"./index.e226f8db.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
