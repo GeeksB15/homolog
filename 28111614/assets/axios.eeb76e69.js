3e9751aab5733ff268a866687782676bec56e7aa
Jean P Franco <jnpefo@gmail.com>
2025-11-28 16:15:57 -0300

    2025.1128.1614

---
+++ 28111614/assets/axios.eeb76e69.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.df2d97b5.js";import{a as e}from"./index.df2d97b5.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 28111614/assets/optidados-white.e4237b1d.js
@@ -0,0 +1,1 @@
+var o="/homolog/28111614/assets/optidados-white.63886fd7.svg";export{o as _};
