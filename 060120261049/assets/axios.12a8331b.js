e6be0f619e09aabaf7955c982aee70564c7e06d7
Jean P Franco <jnpefo@gmail.com>
2026-01-07 09:45:53 -0300

    2026.0107.0943

--- 060120261049/assets/axios.12a8331b.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.2d4c1841.js";import{a as e}from"./index.2d4c1841.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- 060120261049/assets/router.03e658ec.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.2d4c1841.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
