6b377525cd7bec8161c1f911c9e9f4e450aea3b4
Jean P Franco <jnpefo@gmail.com>
2026-01-23 16:49:05 -0300

    2026.0123.1647

---
+++ 021220251654/assets/g.44455af1.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.dd2d9adb.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ 021220251654/assets/router.a67f4d66.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.dd2d9adb.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
