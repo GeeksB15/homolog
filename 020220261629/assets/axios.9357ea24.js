631263aac7f73ac4e2b4904e3a8590a62a4e7328
Lucas <lucassouzacps19@gmail.com>
2026-02-02 16:30:55 -0300

    2026.0202.1629

---
+++ 020220261629/assets/axios.9357ea24.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.fc4a684e.js";import{a as e}from"./index.fc4a684e.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 020220261629/assets/optidados-white.e4237b1d.js
@@ -0,0 +1,1 @@
+var o="/homolog/020220261629/assets/optidados-white.63886fd7.svg";export{o as _};
