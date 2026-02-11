a801c122bc9a8fdf7d1b47ec3cf917e059c59416
Lucas <lucassouzacps19@gmail.com>
2026-02-11 16:32:32 -0300

    2026.0211.1628

---
+++ 110220261628/assets/produto2.fe0b4091.js
@@ -0,0 +1,1 @@
+var o="/homolog/110220261628/assets/produto2.7b99a26f.jpg";export{o as _};
---
+++ 110220261628/assets/router.7a13d1b4.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.5f8e6182.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
