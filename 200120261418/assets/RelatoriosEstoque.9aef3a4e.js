ec0ebd7f56a97ba4ed714430435fe16397434872
Jean P Franco <jnpefo@gmail.com>
2026-02-19 10:52:55 -0300

    2026.0219.1050

--- 200120261418/assets/RelatoriosEstoque.9aef3a4e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,p as t}from"./index.b6af086c.js";const s={components:{},data(){return{data:""}}},a={class:"estoque row"};function r(c,n,_,p,d,u){return o(),t("div",a,"estoque")}var f=e(s,[["render",r]]);export{f as default};
---
+++ 200120261418/assets/g.faa45085.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.a966c897.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
