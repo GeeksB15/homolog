57b8bd5cc35f011bd2021a94f5f5225b1a6eedd9
Jean P Franco <jnpefo@gmail.com>
2026-02-23 14:56:17 -0300

    2026.0223.1454

--- 19022261453/assets/axios.52711f87.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.72ef594a.js";import{a as e}from"./index.72ef594a.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 19022261453/assets/utils.c22fe86f.js
@@ -0,0 +1,1 @@
+import{b as s,aH as l}from"./index.82bc8298.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
