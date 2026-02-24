359983dff583fe9bf7141fe38ebd256231554955
Jean P Franco <jnpefo@gmail.com>
2026-02-24 12:23:37 -0300

    2026.0224.1221

--- 19022261453/assets/axios.cb176728.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.82bc8298.js";import{a as e}from"./index.82bc8298.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
