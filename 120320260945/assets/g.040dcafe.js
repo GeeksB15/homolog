b0f698379d1e88491d43d70559715e2c53dc9dec
Lucas <lucassouzacps19@gmail.com>
2026-03-12 09:47:38 -0300

    2026.0312.0945

---
+++ 120320260945/assets/g.040dcafe.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.9855384c.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ 120320260945/assets/produto5.08fbeead.js
@@ -0,0 +1,1 @@
+var o="/homolog/120320260945/assets/produto5.1f88136e.jpg";export{o as _};
