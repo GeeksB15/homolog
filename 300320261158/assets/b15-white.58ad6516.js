d29fd5341ff5c1330e9ce7785a593516fa9ca8f9
Jean P Franco <jnpefo@gmail.com>
2026-03-30 12:00:48 -0300

    2026.0330.1158

---
+++ 300320261158/assets/b15-white.58ad6516.js
@@ -0,0 +1,1 @@
+var s="/homolog/300320261158/assets/b15-white.2120e95f.svg";export{s as _};
---
+++ 300320261158/assets/g.eb761eb3.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.833ec5f0.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
