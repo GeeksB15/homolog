bdb49f7cb703f920981a56fe6236f6e0d5091017
Lucas <lucassouzacps19@gmail.com>
2026-04-02 10:38:57 -0300

    2026.0402.1037

--- 010420262323/assets/axios.6aee369c.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.61c5046a.js";import{a as e}from"./index.61c5046a.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
