df8fcd1dfa6899c41ea78321bbb158afff71605d
Lucas <lucassouzacps19@gmail.com>
2026-04-08 15:43:54 -0300

    2026.0408.1539

---
+++ 080420261540/assets/axios.edbd68a8.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.d279385a.js";import{a as e}from"./index.d279385a.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 080420261540/assets/utils.a45f6703.js
@@ -0,0 +1,1 @@
+import{b as s,aH as l}from"./index.d279385a.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
