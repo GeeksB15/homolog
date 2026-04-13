f3623b2510be422ae57d403f0188a580c39d775f
Jean P Franco <jnpefo@gmail.com>
2026-04-13 12:17:47 -0300

    2026.0413.1215

---
+++ 130420261215/assets/axios.de8a4e1e.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.41c5bd5b.js";import{a as e}from"./index.41c5bd5b.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 130420261215/assets/produto2.fe0b4091.js
@@ -0,0 +1,1 @@
+var o="/homolog/130420261215/assets/produto2.7b99a26f.jpg";export{o as _};
