c2a02075e01b487792eaac4eba125e96d417d16f
Jean P Franco <jnpefo@gmail.com>
2026-04-16 10:46:06 -0300

    2026.0416.1044

---
+++ 150420261023/assets/axios.5e2fa7fb.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.11d1c18d.js";import{a as e}from"./index.11d1c18d.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
