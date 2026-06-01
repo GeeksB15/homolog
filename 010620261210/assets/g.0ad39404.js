8b42cd891063dc9b7ef2c5ef12c84e30fb6b1a43
Jean P Franco <jnpefo@gmail.com>
2026-06-01 12:12:25 -0300

    2026.0601.1210

---
+++ 010620261210/assets/g.0ad39404.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.2c3bd744.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ 010620261210/assets/produto2.fe0b4091.js
@@ -0,0 +1,1 @@
+var o="/homolog/010620261210/assets/produto2.7b99a26f.jpg";export{o as _};
