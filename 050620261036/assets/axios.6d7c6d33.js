9bfaf8a2af0c747f01d71b9cf75be4ef7dd8abed
Lucas <lucassouzacps19@gmail.com>
2026-06-05 14:46:50 -0300

    2026.0605.1444

--- 050620261036/assets/axios.6d7c6d33.js
+++
@@ -1,1 +0,0 @@
-import{b as s,a as o}from"./index.a6b8326a.js";import{a as e}from"./index.a6b8326a.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- 050620261036/assets/g.f3952b1a.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.a6b8326a.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
