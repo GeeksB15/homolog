a42b3ee8884631012afe8ee68136d357708e3f8d
Lucas <lucassouzacps19@gmail.com>
2026-06-05 14:38:20 -0300

    2026.0605.1435

--- 050620261036/assets/g.78927849.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.f3fcc34f.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ 050620261036/assets/g.f3952b1a.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.a6b8326a.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
