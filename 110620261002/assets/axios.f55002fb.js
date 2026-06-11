317012a27dff55c86c6b92b6d2910691e97ac390
Jean P Franco <jnpefo@gmail.com>
2026-06-11 10:04:08 -0300

    2026.0611.1002

---
+++ 110620261002/assets/axios.f55002fb.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.23c071bf.js";import{a as e}from"./index.23c071bf.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ 110620261002/assets/produto5.08fbeead.js
@@ -0,0 +1,1 @@
+var o="/homolog/110620261002/assets/produto5.1f88136e.jpg";export{o as _};
