1440b1ac33432d7089db1b454b19448c436cc565
Jean P Franco <jnpefo@gmail.com>
2026-06-12 14:54:48 -0300

    2026.0612.1453

---
+++ 120620261417/assets/axios.cf3e8210.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.d240bb51.js";import{a as e}from"./index.d240bb51.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
