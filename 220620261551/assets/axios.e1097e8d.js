49cfecf79721158ee3b5efc938557fc98056f2cb
Patrick <eng.patrickmonteiro@gmail.com>
2026-06-23 14:43:25 -0300

    2026.0623.1442

---
+++ 220620261551/assets/axios.e1097e8d.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.27fb23f3.js";import{a as e}from"./index.27fb23f3.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- 220620261551/assets/g.2bac57fa.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.83fe9fd9.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
