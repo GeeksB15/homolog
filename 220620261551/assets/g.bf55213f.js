cf64689c45fa1cc1f7354bf39e426973280b6cef
Patrick <eng.patrickmonteiro@gmail.com>
2026-06-24 17:23:23 -0300

    2026.0624.1722

--- 220620261551/assets/g.bf55213f.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.b5102611.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- 220620261551/assets/utils.16a3f347.js
+++
@@ -1,1 +0,0 @@
-import{b as s,aH as l}from"./index.b5102611.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
