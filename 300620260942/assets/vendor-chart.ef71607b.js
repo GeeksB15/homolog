ef17237bdd77f68c572be432a9ee86d8173e8372
Patrick <eng.patrickmonteiro@gmail.com>
2026-07-14 15:13:35 -0300

    2026.0714.1512

--- 300620260942/assets/vendor-chart.ef71607b.js
+++ 300620260942/assets/vendor-chart.ef71607b.js
@@ -1,4 +1,4 @@
-import{c as Xi}from"./index.b8ee39fc.js";var Yr={exports:{}};/*!
+import{c as Xi}from"./index.534058db.js";var Yr={exports:{}};/*!
  * Chart.js v2.9.4
  * https://www.chartjs.org
  * (c) 2020 Chart.js Contributors
