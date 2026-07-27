a1399495603a943d4ef59f1a34f159afde444571
Lucas <lucassouzacps19@gmail.com>
2026-07-27 15:03:20 -0300

    2026.0727.1501

--- 270720261419/assets/vendor-chart.46c18240.js
+++ 270720261419/assets/vendor-chart.46c18240.js
@@ -1,4 +1,4 @@
-import{c as Xi}from"./index.c40f0b6d.js";var Yr={exports:{}};/*!
+import{c as Xi}from"./index.1f402a48.js";var Yr={exports:{}};/*!
  * Chart.js v2.9.4
  * https://www.chartjs.org
  * (c) 2020 Chart.js Contributors
