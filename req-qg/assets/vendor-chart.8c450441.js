f549e883654ad64f53924ad19f7822c5c06a3154
Maicon Dias <maiconhenriquedias@gmail.com>
2026-09-22 16:39:25 -0300

    2026.0922.1638

--- req-qg/assets/vendor-chart.8c450441.js
+++ req-qg/assets/vendor-chart.8c450441.js
@@ -1,4 +1,4 @@
-import{c as Xi}from"./index.e6352f2a.js";var Yr={exports:{}};/*!
+import{c as Xi}from"./index.52cd3afd.js";var Yr={exports:{}};/*!
  * Chart.js v2.9.4
  * https://www.chartjs.org
  * (c) 2020 Chart.js Contributors
